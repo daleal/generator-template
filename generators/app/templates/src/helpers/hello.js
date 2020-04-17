0e9756f34265292f6c6cb81e6d37d48b735ae4b9
Daniel Leal <dlleal@uc.cl>
2020-04-17 16:28:41 -0400

    Add example helper module

---
+++ generators/app/templates/src/helpers/hello.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  // Variable :name must exist and determines the helper namespace
+  name: 'hello',
+  // Write your functions
+  sayHey: () => {
+    console.log('Hey'); // eslint-disable-line no-console
+  },
+  sayHo: () => {
+    console.log('Ho'); // eslint-disable-line no-console
+  },
+};
